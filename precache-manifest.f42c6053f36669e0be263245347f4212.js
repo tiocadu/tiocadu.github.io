eac4e6a10faa1cffbf03fdad0d4227c9520c6f89
Carlos Eduardo <edugaspar@hotmail.com>
2019-12-16 01:14:01 -0300

    first try deploying react app

---
+++ precache-manifest.f42c6053f36669e0be263245347f4212.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ccc313300b1d038e66a927ffe0824bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ccad3386700f7c47717",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f788bf5ac3cfe9d0904e",
+    "url": "/static/js/2.53926f2e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.53926f2e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ccad3386700f7c47717",
+    "url": "/static/js/main.5d336b1c.chunk.js"
+  },
+  {
+    "revision": "d1de4891002ebe476257",
+    "url": "/static/js/runtime-main.d5c7476b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
